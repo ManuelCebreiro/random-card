91c7e19fe36f26f189d2bcb48d3132ad9f13473e
ManuelCebreiro <cebreirom@gmail.com>
2022-06-24 01:33:43 +0000

    Ejercicio en proceso de arreglar

---
+++ app.js
@@ -0,0 +1,45 @@
+window.onload = function (){
+    cartas();
+}
+
+function cartas(){
+    let simbols = ["♦", "♥", "♠", "♣"];
+    let values = ["A", "2","3","4","5","6","7","8","9","10","J","Q","K"];
+    let simbolsrandom = randomelection(simbols.length); //posiciones simbolos
+    let valuesrandom = randomelection(values.length); //posiciones del valor de la carta
+    let numeroaleatorio = values[valuesrandom]; //numero de la carta aleatorio incluyendo letras
+    let simbolsaleatorio = simbols[simbolsrandom]; //simbolo aleatorio
+    
+    function randomelection(arr){
+    return Math.floor(Math.random()* arr);}
+
+    let a = document.querySelector("#number").innerHTML= numeroaleatorio;  //numero o letra de carta aleatorio
+    let b = document.querySelector("#simbolo").innerHTML= simbolsaleatorio; // simbolos de arriba
+    let c = document.querySelector("#simbol1").innerHTML= simbolsaleatorio; // simbolos de abajo
+
+    //document.getElementById("#simbol").style.color = "red";
+
+    let x= document.getElementById("simbolo")
+    let y= document.getElementById("simbol1")
+
+    console.log(a,b)
+
+b === "♥" || c == "♦" ? x.style.color = "red": x.style.color = "black";
+b === "♥" || c == "♦" ? y.style.color = "red": y.style.color = "black";
+    }
+
+//
+
+let btn = document.getElementById("clickme")
+
+  btn.addEventListener("click", function(){
+    cartas()
+})
+
+
+//if(b == "♥" || c == "♥"){
+    //x.style.color = "red"
+    //y.style.color = "red"}
+
+
+ 
